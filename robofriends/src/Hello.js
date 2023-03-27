e5b62d53230b7fe0574966eb271f1e0469bc9229
Maurice Sattrasai <maurice.sattrasai@hotmail.com>
2023-03-27 14:45:26 +0200

    added Hello Component

--- robofriends/src/Hello.js
+++ robofriends/src/Hello.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import './Hello.css';
 
 class Hello extends React.Component{
   render() {
-    return <h1>Hello World</h1>
+    return(
+    <div>
+      <h1>Hello World</h1>
+      <p>What's up</p>
+    </div>
+    )
   }
 }
 
