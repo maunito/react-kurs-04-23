d8c78514572128b08d953caa0bbfae0363516e89
Maurice Sattrasai <maurice.sattrasai@hotmail.com>
2023-03-28 11:35:09 +0200

    added scroll function so that the search bar always stays on top

--- robofriends/src/App.js
+++ robofriends/src/App.js
@@ -1,17 +1,23 @@
 
 import React from 'react';
 import CardList from './CardList'
-import {robots} from './robots'
 import SearchBox from './SearchBox'
+import Scroll from './Scroll'
 
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      robots: robots, 
+      robots: [], 
       searchfield: ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({robots: users}))
   }
 
   onSearchChange = (event) => {
@@ -24,13 +30,20 @@ class App extends React.Component {
     const filteredRobots = this.state.robots.filter(robots => {
       return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
     })
-    return (
-      <div>
-        <h1>Robofriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filteredRobots}/> 
-      </div>
-    );
+    if (this.state.robots.length === 0 ) {
+      return <h1>Loading..</h1>
+    } else {
+      return (
+        <div>
+          <h1>Robofriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <CardList robots={filteredRobots}/> 
+          </Scroll>
+        </div>
+      );
+    }
+
   }
   
 }
