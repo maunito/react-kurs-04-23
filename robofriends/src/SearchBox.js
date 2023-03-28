2aa7d68490885dc249b5f6b6e59ba035fd5cd723
Maurice Sattrasai <maurice.sattrasai@hotmail.com>
2023-03-28 10:57:58 +0200

    search function is now working

--- robofriends/src/SearchBox.js
+++ robofriends/src/SearchBox.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({searchfield, searchChange}) => {
   return (
     <div>
       <input
         type='search' 
         placeholder='search robots'
+        onChange={searchChange}
       />
     </div>
     
